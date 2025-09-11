f52f61533b9d49c5fae821d29f4e0a008feac102
LillKonst <konstlillkristine@gmail.com>
2025-09-11 19:27:57 +0200

    working on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import { RouteNotFound } from "./components/Layout/Header/Header";
 import Layout from "./components/Layout/Layout";
 import Home from "./routes/Home/Home";
+import About from "./routes/About/About";
+import Contact from "./routes/Contact/Contact";
+import Products from "./routes/Products/Products";
 
 function App() {
   
 
   return (
-    <div>
+    <div className="bg-background h-screen p-5">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="" element={<Home />} />
+          <Route path="" element={<Products />} />
+          <Route path="" element={<About />} />
+          <Route path="" element={<Contact />} />
           <Route path="*" element={<RouteNotFound />} />
         </Route>
       </Routes>
--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,11 +1,20 @@
+import logo from "../../../images/logo.svg"
+
 export function RouteNotFound() {
   return <div>Page not found</div>;
 }
 
 export default function Header() {
   return (
-    <div>
-      Logo
+    <div className="p-5 flex items-center justify-between">
+      <img src={logo} alt="Spor 17 logo"/>
+      <nav>
+        <ul className="flex gap-4">
+          <li>Julekort</li>
+          <li>Om oss</li>
+          <li>Kontakt</li>
+        </ul>
+      </nav>
     </div>
   );
 }
