c17027866853cf5b9e487d9775c8cb4eb8ba79d7
LillKonst <konstlillkristine@gmail.com>
2025-10-26 13:04:27 +0100

    added productslider to homepage

--- src/components/CartItemList/CartItemList.tsx
+++ src/components/CartItemList/CartItemList.tsx
@@ -1,21 +1,3 @@
-// import type { CartLine } from "../../hooks/shopifyCart";
-// import CartItem from "../CartItem/CartItem";
-
-// interface CartItemListProps {
-//   lines: CartLine[];
-//   onUpdateQuantity: (lineId: string, quantity: number) => void;
-// }
-
-// export default function CartItemList({ lines, onUpdateQuantity}: CartItemListProps) {
-//   return (
-//     <ul className="flex flex-col gap-5">
-//       {lines.map((line) => (
-//         <CartItem key={line.id} line={line} onUpdateQuantity={onUpdateQuantity} />
-//       ))}
-//     </ul>
-//   );
-// }
-
 import type { CartLine } from "../../hooks/shopifyCart";
 import CartItem from "../CartItem/CartItem";
 
--- src/components/ProductLink/ProductLink.tsx
+++ src/components/ProductLink/ProductLink.tsx
@@ -9,16 +9,18 @@ export default function ProductLink({ product }: ProductLinkProps) {
   const image = product.images.edges[0]?.node;
 
   return (
-    <li key={product.id} className="transition-transform ease-out duration-200 hover:scale-102 rounded-lg p-2 md:p-4 bg-white flex flex-col shadow-lg">
+    <li key={product.id} className="transition-transform ease-out duration-200 hover:scale-102 rounded-lg bg-white flex flex-col shadow-lg">
             <Link to={`/product/${product.handle}`}>
+            <div className="w-full aspect-[5/3.5] rounded-lg overflow-hidden">
             {product.images.edges[0] && (
               <img
                 src={image.url}
                 alt={image.altText || product.title}
-                className="w-full rounded-lg -my-5 px-3"
+                className="w-full h-full object-cover"
               />
             )}
-            <div className="-mt-5 xxs:-mt-10 md:-mt-5 mx-4 px-1 flex flex-col justify-center">
+            </div>
+            <div className=" mx-4 py-5 flex flex-col justify-center">
           <h2 className="text-md xs:text-xl">
             {product.title.slice(0, 10)}
             <br />
