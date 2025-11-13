d05f011bcbef7ba745dc156f3faf9493cf9b7c81
LillKonst <konstlillkristine@gmail.com>
2025-11-13 15:01:49 +0100

    deletet analytics.ts

--- src/analytics.ts
+++
@@ -1,9 +0,0 @@
-import ReactGA from "react-ga4";
-
-export const initGA = () => {
-  ReactGA.initialize("G-CJ2ETBFSC2"); 
-};
-
-export const trackPageView = (path: string) => {
-  ReactGA.send({ hitType: "pageview", page: path });
-};
