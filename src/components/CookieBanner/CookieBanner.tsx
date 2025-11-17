188c8887922a404d3b1989e6be2602ef4fb329a8
LillKonst <konstlillkristine@gmail.com>
2025-11-17 12:45:55 +0100

    cookie-consent html

--- src/components/CookieBanner/CookieBanner.tsx
+++ src/components/CookieBanner/CookieBanner.tsx
@@ -2,6 +2,25 @@ import CookieConsent from "react-cookie-consent";
 import { Link } from "react-router-dom";
 
 export default function CookieBanner() {
+   const handleAccept = () => {
+    // Trigger Google Analytics consent
+    window.gtag?.('consent', 'update', {
+      'analytics_storage': 'granted',
+      'ad_storage': 'granted'
+    });
+    window.gtag?.('event', 'page_view');
+
+    // Trigger Meta Pixel eller andre scripts her hvis nødvendig
+    window.dispatchEvent(new Event("ga-consent-given"));
+  };
+
+  const handleDecline = () => {
+    window.gtag?.('consent', 'update', {
+      'analytics_storage': 'denied',
+      'ad_storage': 'denied'
+    });
+    window.dispatchEvent(new Event("ga-consent-denied"));
+  };
   return (
     <CookieConsent
       location="bottom"
@@ -9,8 +28,8 @@ export default function CookieBanner() {
       declineButtonText="Avslå"
       enableDeclineButton
       cookieName="spor17-consent"
-      onAccept={() => window.dispatchEvent(new Event("ga-consent-given"))}
-      onDecline={() => window.dispatchEvent(new Event("ga-consent-denied"))}
+      onAccept={handleAccept}
+      onDecline={handleDecline}
       style={{ background: "#F7F7F7", color: "#000000" }}
       buttonStyle={{ background: "#A5EB9F", color: "#000000", fontSize: "14px" }}
       declineButtonStyle={{ background: "#777", color: "#fff", fontSize: "14px" }}
