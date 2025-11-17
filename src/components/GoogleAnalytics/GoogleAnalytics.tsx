f794722f1736dd384ce41d4263036605a74badc8
LillKonst <konstlillkristine@gmail.com>
2025-11-17 10:30:58 +0100

    fixed googleanalytics

--- src/components/GoogleAnalytics/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics/GoogleAnalytics.tsx
@@ -1,47 +1,43 @@
-// src/components/GoogleAnalytics/GoogleAnalytics.tsx
 import { useEffect } from "react";
 
 declare global {
   interface Window {
-    dataLayer?: unknown[];
-    gtag?: (...args: unknown[]) => void;
+    dataLayer: unknown[];
+    gtag: (...args: unknown[]) => void;
   }
 }
 
-interface GoogleAnalyticsProps {
-  measurementId: string;
-}
 
-export default function GoogleAnalytics({ measurementId }: GoogleAnalyticsProps) {
+export default function GoogleAnalytics({ measurementId }: { measurementId: string }) {
   useEffect(() => {
-  function initGA() {
-     if (!window.dataLayer) {
-      window.dataLayer = [];
+    function initGA() {
+      if (window.dataLayer) return;
 
+      window.dataLayer = [];
       window.gtag = function (...args: unknown[]) {
         window.dataLayer!.push(args);
       };
-
-      window.gtag("js", new Date());
-      window.gtag("config", measurementId);
 
       const script = document.createElement("script");
       script.async = true;
       script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
       document.head.appendChild(script);
+
+      window.gtag("js", new Date());
+      window.gtag("config", measurementId);
     }
-  }
 
-  const cookie = document.cookie.includes("spor17-consent=1");
-  if (cookie) initGA();
+    // Hvis cookie finnes (uansett verdi)
+    const cookie = document.cookie.includes("spor17-consent");
+    if (cookie) initGA();
 
-  window.addEventListener("ga-consent-given", initGA);
+    // Hvis bruker trykker på godta-knappen
+    window.addEventListener("ga-consent-given", initGA);
 
-  return () => {
-    window.removeEventListener("ga-consent-given", initGA);
-  };
-}, [measurementId]);
-
+    return () => {
+      window.removeEventListener("ga-consent-given", initGA);
+    };
+  }, [measurementId]);
 
   return null;
 }
