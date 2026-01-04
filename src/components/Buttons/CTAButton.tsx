807b20de26f79088b31dc934ce4dbc667f599ae0
LillKonst <konstlillkristine@gmail.com>
2026-01-04 18:03:07 +0100

    main image homepage

---
+++ src/components/Buttons/CTAButton.tsx
@@ -0,0 +1,26 @@
+// components/CTAButton/CTAButton.tsx
+interface CTAButtonProps {
+  label: string;
+  onClick: () => void;
+  loading?: boolean;
+  disabled?: boolean;
+  className?: string;
+}
+
+export default function CTAButton({
+  label,
+  onClick,
+  loading = false,
+  disabled = false,
+  className,
+}: CTAButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={loading || disabled}
+      className={`bg-ctaPink hover:bg-customPink text-customWhite text-xl p-3 px-4 rounded ${className || ""}`}
+    >
+      {loading ? "Laster..." : label}
+    </button>
+  );
+}
