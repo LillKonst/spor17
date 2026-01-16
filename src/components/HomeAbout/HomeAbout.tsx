473a0caacf214b4c844af96d43ac3869b19f4fb6
LillKonst <konstlillkristine@gmail.com>
2026-01-16 23:28:12 +0100

    added arrow to HomeAbout

--- src/components/HomeAbout/HomeAbout.tsx
+++ src/components/HomeAbout/HomeAbout.tsx
@@ -20,7 +20,11 @@ export default function HomeAbout() {
           Jeg heter Lill og lager illustrerte kort til små og store øyeblikk.
           
         </p>
-        <p className="text-lg md:text-2xl lg:mt-10">Vil du vite mer om Spor 17?</p>
+        <p className="text-lg md:text-2xl lg:mt-10 flex gap-2 items-center">Vil du vite mer om Spor 17? 
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+</svg>
+        </p>
         
       </Link>
       
