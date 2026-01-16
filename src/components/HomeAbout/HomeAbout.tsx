c05ba78377f5a0facaa530cc8028e60fa8636c94
LillKonst <konstlillkristine@gmail.com>
2026-01-16 21:05:10 +0100

    changed colors on HomeAbout

--- src/components/HomeAbout/HomeAbout.tsx
+++ src/components/HomeAbout/HomeAbout.tsx
@@ -14,7 +14,7 @@ export default function HomeAbout() {
 
     <div className="flex flex-col gap-5 md:flex-1 md:h-[400px] lg:h-[500px]">
       <Link 
-          to="/om"  className=" flex flex-col justify-center bg-customBlue hover:scale-103 rounded p-5 h-full lg:py-10">
+          to="/om"  className=" flex flex-col justify-center bg-customPink hover:scale-103 rounded p-5 h-full lg:py-10">
         <h2 className="text-2xl lg:text-4xl mb-5">Hei – så fint at du er her 💗</h2>
         <p className="lg:mb-4 text-lg md:text-2xl">
           Jeg heter Lill og lager illustrerte kort til små og store øyeblikk.
@@ -26,7 +26,7 @@ export default function HomeAbout() {
       
       <Link 
           to="/kontakt" 
-          className="bg-ctaPink hover:scale-103 text-white px-6 py-3 rounded lg:py-12 text-lg md:text-2xl transition text-center"
+          className="border-2 border-ctaPink hover:scale-103 text-customBlack px-6 py-3 rounded lg:py-12 text-lg md:text-2xl transition text-center"
         >
           Eller stille et spørsmål?
         </Link>
